95134e92d025ab449d964c8e9ba667a8953f7ea0
Justin Beckwith <justin.beckwith@gmail.com>
2016-03-10 22:44:08 -0800

    show images on publish

--- web/public/script.js
+++ web/public/script.js
@@ -3,10 +3,60 @@ var nub = PUBNUB.init({
   ssl: true
 });
 
+var dogs = 0;
+var cats = 0;
+var other = 0;
+var total = 0;
+
+var dogDiv = document.getElementById('dogs');
+var catDiv = document.getElementById('cats');
+
 nub.subscribe({
   channel: 'cloudcats',
   message: function(m) {
     console.log(m);
+    
+    if (m.data.type === 'fin') {
+      total = m.data.total;
+      console.log('total!: ' + total);
+    } else {
+      var pic = document.createElement('div');
+      pic.style.backgroundImage = 'url(' + m.data.url + ')';
+      var container = null;
+      switch (m.data.type) {
+        case "other":
+          other++;
+          break;
+        case "dog":
+          dogs++;
+          dogDiv.insertBefore(pic, dogDiv.firstChild);
+          break;
+        case "cat":
+          cats++;
+          catDiv.insertBefore(pic, catDiv.firstChild);
+          break;
+      }
+    }
+
+    if (other + dogs + cats === total) {
+      var winner;
+      console.log('we are DONE');
+      if (cats > dogs) {
+        winner = 'CATS';
+      } else if (dogs > cats) {
+        winner = 'DOGS';
+      } else {
+        winner = 'tie';
+      }
+
+      showDialog({
+        winner: winner,
+        dogs: dogs,
+        cats: cats,
+        other: other,
+        total: total
+      });
+    }
   }
 });
 
@@ -18,23 +68,32 @@ addButton.addEventListener('click', function() {
   main.classList.add('cover2');
   main.classList.remove('cover');
   progress.style.display = 'block';
-  //fetch('http://localhost:8081/go', { mode: 'no-cors' });
+  dogs = 0;
+  cats = 0;
+  other = 0;
+  total = 0;
+  fetch('http://localhost:8081/go', { mode: 'no-cors' });
   snackbar.MaterialSnackbar.showSnackbar({
     message: "Let's get started!"
   });
-  setTimeout(showDialog, 2000);
 });
 
 var dialog = document.querySelector('dialog');
 dialog.querySelector('.close').addEventListener('click', function() {
   dialog.close();
 });  
-function showDialog() {
+
+function showDialog(data) {
   var dialogContent = document.getElementById('dialogContent');
   var dialogTitle = document.getElementById('dialogTitle');
-  dialogContent.innerText = 'It looks like the Internet really likes cats. Who knew?';
-  dialogTitle.innerText = 'CATS WIN!'
+
+  if (data.winner == 'tie') {
+    dialogContent.innerText = 'There was a tie!? That was unfulfilling.';
+    dialogTitle.innerText = 'TIE! TIE! TIE!'  
+  } else {
+    dialogContent.innerText = 'It looks like the Internet really likes ' + data.winner + '. Who knew?';
+    dialogTitle.innerText = data.winner + ' WINS!'
+  }
   progress.style.display = 'none';
   dialog.showModal();
 }
-
